b3a07a50edef254a07a28f207c6d4c10f6de6613
aar4uu <aarjupawara1125@gmail.com>
2024-11-28 12:59:23 +0530

    Add files via upload

---
+++ L9-101.C
@@ -0,0 +1,10 @@
+//PROGRAM 101
+#include<stdio.h>
+int main()
+{
+    FILE *fptr;
+    fptr = fopen("LNMIITSTUDENT.DAT","a");
+    fprintf(fptr,"%s","This file is on append Mode");
+    fclose(fptr);
+    return 0;
+}
